1c54375ed395c002970fb911be93b36c45d364d3
Angel Dimitrov <52366403+angeldimitrov94@users.noreply.github.com>
2025-06-20 01:53:30 +0000

    Add templating, change to black linkedin logo

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+//router
+const routes = {
+    '': 'home.html',
+    '#about': 'about.html',
+    '#projects': 'projects.html',
+    '#contact': 'contact.html'
+};
+
 function markActive() {
     const current = 0;
     for (var i = 0; i < document.links.length; i++) {
@@ -7,3 +15,30 @@ function markActive() {
     }
     document.links[current].className = 'current';
 }
+
+function loadTemplate() {
+    //load header
+    fetch('header.html').
+        then(r => r.text()).
+        then(html => document.getElementById('header').innerHTML = html);
+    //load footer
+    fetch('footer.html').
+        then(r => r.text()).
+        then(html => document.getElementById('footer').innerHTML = html);
+}
+
+function loadContent() {
+    const path = window.location.hash || '';
+    const page = routes[path] || '404.html';
+    fetch(page)
+        .then(r => r.text())
+        .then(html => {
+            document.getElementById('content').innerHTML = html;
+        });
+}
+
+window.addEventListener('hashchange', loadContent);
+window.addEventListener('DOMContentLoaded', () => {
+    loadContent();
+    loadTemplate();
+});
